d415b84acc2772f39d34c973b340980f26bcd774
andiLeong <alwayssmile20150724@gmail.com>
2022-06-11 19:57:23 +0800

    extract a logout component and 2 svg - sun and moon component

---
+++ src/components/SignOut.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <a
+        @click.prevent="logout"
+        href="#"
+        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
+    >
+        Sign out
+    </a>
+</template>
+
+<script setup>
+import {useStore} from "vuex";
+
+defineProps(['active'])
+const store = useStore();
+
+function logout() {
+    axios
+        .post(`/logout`)
+        .then((response) => {
+            console.log(response);
+            store.dispatch('logout');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+</script>
+
+<style scoped>
+
+</style>
