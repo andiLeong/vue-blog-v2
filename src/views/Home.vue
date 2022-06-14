9865c1c0cefc8a9759617a7b7a2406405392fe15
andiLeong <alwayssmile20150724@gmail.com>
2022-06-14 15:12:29 +0800

    make a developer bio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,43 +16,55 @@
                     <div class="px-4 py-2 relative dark:bg-gray-600 rounded bg-zinc-100 md:p-6">
                         <div class="lg:grid lg:grid-cols-2 lg:gap-6">
                             <div class="prose prose-indigo prose-lg text-gray-500 dark:text-white lg:max-w-none">
-                              <p>
-                                    Ultrices ultricies a in odio consequat egestas rutrum. Ut
-                                    vitae aliquam in ipsum. Duis nullam placerat cursus risus
-                                    ultrices nisi, vitae tellus in. Qui non fugiat aut minus aut
-                                    rerum. Perspiciatis iusto mollitia iste minima soluta id.
-                                </p>
                                 <p>
-                                    Amet, eu nulla id molestie quis tortor. Auctor erat justo, sed
-                                    pellentesque scelerisque interdum blandit lectus. Nec viverra
-                                    amet ac facilisis vestibulum. Vestibulum purus nibh ac
-                                    ultricies congue.
+                                    A 2 years full stack php <span class="text-red-500 font-semibold dark:text-red-400">laravel</span> +
+                                    <span class="text-green-500 font-semibold">vue</span> web developer.
+                                    Actually this blog was built on top of laravel + vue. Make use of laravel act as
+                                    backend,
+                                    utilities vue router to make SPA to improve user experience.
+                                    For the style I love using utility css framework tailwind.
                                 </p>
+
+                                <h3 class="dark:text-white">Skills</h3>
+                                <div>
+                                    <div v-for="skill in skills" :key="skill" class="flex items-center">
+                                        <div class="mr-2">
+                                            <svg class="h-6 w-6 text-sky-400" xmlns="http://www.w3.org/2000/svg"
+                                                 fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                 aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                            </svg>
+                                        </div>
+                                        <div>{{ skill }}</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div
-                                class="mt-6 prose prose-indigo prose-lg text-gray-500 dark:text-white lg:mt-0"
-                            >
+                            <div class="mt-6 prose prose-indigo prose-lg text-gray-500 dark:text-white lg:mt-0">
+                                <h3 class="dark:text-white">Advantage</h3>
+
+                                <div class="space-y-2">
+                                    <div v-for="advantage in advantages" :key="advantage"> {{advantage}}</div>
+                                </div>
+
+                                <h3  class="dark:text-white">Experience</h3>
                                 <p>
-                                    Erat pellentesque dictumst ligula porttitor risus eget et
-                                    eget. Ultricies tellus felis id dignissim eget. Est augue
-                                    maecenas risus nulla ultrices congue nunc tortor.
+                                    Maintain and develop several projects from previous company, that includes a small
+                                    ecommerce site,
+                                    a job posting site, A social insurance buying platform, a construction monitor
+                                    platform
                                 </p>
+
+                                <h3  class="dark:text-white">Code Review</h3>
                                 <p>
-                                    Eu leo risus porta integer suspendisse sed sit ligula elit.
-                                    Elit egestas lacinia sagittis pellentesque neque dignissim
-                                    vulputate sodales. Diam sed mauris felis risus, ultricies
-                                    mauris netus tincidunt. Mauris sit eu ac tellus nibh non eget
-                                    sed accumsan. Viverra ac sed venenatis pulvinar elit. Cras
-                                    diam quis tincidunt lectus. Non mi vitae, scelerisque felis
-                                    nisi, netus amet nisl.
+                                    had done some side code project where you can check and view my code style
+                                    including:
                                 </p>
-                                <p>
-                                    Eu eu mauris bibendum scelerisque adipiscing et. Justo,
-                                    elementum consectetur morbi eros, posuere ipsum tortor. Eget
-                                    cursus massa sed velit feugiat sed ut. Faucibus eros mauris
-                                    morbi aliquam nullam. Scelerisque elementum sit magna
-                                    ullamcorper dignissim pretium.
-                                </p>
+                                <div class="space-y-0">
+                                    <div v-for="review in codeReviews" :key="review.name">
+                                        <a class="text-sky-400 dark:text-blue-400" :href="review.url">{{review.name}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -64,6 +76,40 @@
 
 <script setup>
 import {useUserStore} from '@/store/user'
+import {ref} from "vue";
 
 const userStore = useUserStore();
+
+const skills = ref([
+    'native php',
+    'php framework --- laravel, codeigniter',
+    'javascript framework --- vue js',
+    'database -- redis , mysql',
+    'server -- nginx',
+    'version control -- git',
+    'able to use Vim in daily development',
+    'Test Driven Development using phpunit',
+    'A vue echo system , vue router, vuex , pinia ,vite',
+    'alpine js',
+    'tailwind css && bootstrap css',
+    'other laravel community package : livewire , inertia',
+    'deployment LNMP on digitalocean or other vps',
+])
+
+const codeReviews = ref([
+    {name:'collection',url:'https://github.com/andiLeong/collection'},
+    {name:'aliyun sms',url:'https://github.com/andiLeong/aliyunSms'},
+    {name:'a simple e-commerce',url:'https://github.com/andiLeong/my-e-commence'},
+    {name:'a datatable with vue',url:'/order'},
+    {name:'vue paginator',url:'/pagination'}
+])
+
+const advantages = ref([
+    'Has strong oop fundamental concept, aim to write clean , expressive and maintainable codes.',
+    'Understand and use major design principles/pattern, like strategy pattern, tell don\'t ask, solid, decorator, adaptor etc..',
+    'Having a habit refactor code after written aims to make it perfect, not a guy write code then go home.',
+    'Having a moderate level of understanding to laravel, from bootstrap the app, IOC container, Eloquent, service provider, collection etc.. .',
+    'Always write test first, or sometimes later to make sure the application is well tested before going live to reduce unnecessary bugs.',
+    'Always write raw sql to see whats happening without reply on Eloquent too much.'
+])
 </script>
