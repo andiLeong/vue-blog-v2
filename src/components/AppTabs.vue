57f84603ba5956cfd9b8a0742c29e3e22fd89acb
andiLeong <alwayssmile20150724@gmail.com>
2022-06-17 13:24:41 +0800

    make a tab component

--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -1,36 +1,39 @@
 <template>
-  <div>
-    <div class="tabs">
-      <ul>
-        <li
-          v-for="(tab, index) in tabs"
-          :class="{ 'is-active': tab.isActive }"
-          :key="index"
-        >
-          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+    <div class="max-w-6xl mx-auto my-10 space-y-4">
+      <h1 class="text-2xl font-bold ">Tabs</h1>
+      <ul class="flex">
+        <li v-for="(tab,index) in tabs"
+            :key="index">
+          <button
+              :class="{
+                'bg-blue-500' : selectedTitle === tab.props.title,
+                'bg-gray-500' : selectedTitle !== tab.props.title
+              }"
+              class="py-2 px-4 text-white"
+              @click.prevent="selectedTitle = tab.props.title"
+          >
+            {{tab.props.title}}
+          </button>
         </li>
       </ul>
+      <slot ></slot>
     </div>
-
-    <div class="tabs-details">
-      <slot></slot>
-    </div>
-  </div>
 </template>
 
-<script>
-export default {
-  name: 'Tabs',
-  data() {
-    return { tabs: [] };
-  },
-  created() {},
-  methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach((tab) => {
-        tab.isActive = tab.name == selectedTab.name;
-      });
-    },
-  },
-};
+<script setup>
+
+import { provide } from 'vue'
+import { useSlots } from 'vue'
+import {ref,onMounted} from "vue";
+
+const selectedTitle = ref();
+const tabs = ref();
+
+provide('selectedTab', selectedTitle)
+
+onMounted( () => {
+  tabs.value = useSlots().default()
+  selectedTitle.value = useSlots().default()[0].props.title
+})
+
 </script>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,6 +1,5 @@
 <template>
-  <h1 class="text-2xl font-bold mb-6">Tabs</h1>
-  <!-- <AppTabs>
+  <AppTabs>
     <AppTab title="First">
       <p>Hello world.</p>
     </AppTab>
@@ -10,16 +9,11 @@
     <AppTab title="Third">
       <p>Hello world and out.</p>
     </AppTab>
-    <AppTab title="Wew" active>
+    <AppTab title="Wew">
       <p>It works!</p>
     </AppTab>
-  </AppTabs> -->
+  </AppTabs>
 
-  <AppTab> </AppTab>
-
-  <!-- <AppTab title="Wew" active>
-    <p>It works!22222222222222</p>
-  </AppTab> -->
 </template>
 
 <script setup>
