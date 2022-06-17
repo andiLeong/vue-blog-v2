36634463a32c055c07f3943f721f3a20bf8709e0
andiLeong <alwayssmile20150724@gmail.com>
2022-06-17 22:33:42 +0800

    make a load more component for infinitely scrolling

---
+++ src/components/LoadMore.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-observe-visibility="{
+        callback: toBottom,
+        intersection: {
+            threshold : '0.9'
+        },
+    }">
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['load'])
+
+function toBottom(inView) {
+    if (inView) {
+        console.log('bottom now')
+        emit('load')
+    }
+}
+</script>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,96 +1,89 @@
 <template>
-  <div class="pt-10 pb-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
-    <div
-      class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl"
-    >
-      <div
-        v-if="posts.length"
-        class="mt-4 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12"
-      >
-        <InfiniteScroll :items="posts" @refetch="fetch">
-          <template v-slot:item="{ item }">
-            <AppLink
-              class="block mt-4"
-              :to="{ name: 'posts.show', params: { slug: item.slug } }"
+    <div class="pt-10 pb-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
+        <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+            <div
+                v-if="posts.length"
+                class="mt-4 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12 "
             >
-              <p class="text-xl font-semibold text-gray-900 dark:text-white">
-                {{ item.title }}
-              </p>
-              <p class="mt-3 text-base text-gray-500 dark:text-white">
-                {{ item.shortDescription }}
-              </p>
-            </AppLink>
-          </template>
-          <template v-slot:loading>
-            <div v-show="noMorePost === false && fetching === true">
-              <Spinner class="animate-spin -ml-1 mr-3 h-5 w-5 text-sky-600" />
+                <div v-for="post in posts" :key="post.id" class="mb-4">
+                    <AppLink
+                        class="block mt-4"
+                        :to="{ name: 'posts.show', params: { slug: post.slug } }"
+                    >
+                        <p class="text-xl font-semibold text-gray-900 dark:text-white">
+                            {{ post.title }}
+                        </p>
+                        <p class="mt-3 text-base text-gray-500 dark:text-white">
+                            {{ post.shortDescription }}
+                        </p>
+                    </AppLink>
+                </div>
+                <LoadMore @load="loadMore">
+                    <template v-if="page !== lastPage && fetching">
+                        <Spinner class="animate-spin -ml-1 mr-3 h-5 w-5 text-sky-600"/>
+                    </template>
+                </LoadMore>
             </div>
-          </template>
-        </InfiniteScroll>
 
-        <div
-          v-show="noMorePost"
-          class="sm:hidden font-semibold text-transparent bg-clip-text bg-gradient-to-br from-sky-500 to-lime-200 text-center text-sm"
-        >
-          No more post
+            <div v-else class="mt-4 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+                <PostsSkeletons/>
+            </div>
+
+            <div
+                v-if="fetched && posts.length < 1"
+                class="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-sky-500 to-lime-200 text-center text-md"
+            >
+                No post at the moment !
+            </div>
         </div>
-      </div>
-
-      <div
-        v-else-if="noMorePost"
-        class="font-semibold text-transparent bg-clip-text bg-gradient-to-br from-sky-500 to-lime-200 text-center text-md"
-      >
-        No post at the moment !
-      </div>
-
-      <div
-        v-else
-        class="mt-4 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12"
-      >
-        <PostsSkeletons />
-      </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {ref, watch} from 'vue';
 import PostsSkeletons from '@/components/PostsSkeletons.vue';
 import InfiniteScroll from '@/components/InfiniteScroll.vue';
 import AppLink from '@/components/AppLink.vue';
 import Spinner from '@/components/Spinner.vue';
 import { useMeta } from 'vue-meta'
+import LoadMore from '@/components/LoadMore.vue'
 
 const posts = ref([]);
-const lastPage = ref(1);
-const noMorePost = ref(false);
+const lastPage = ref(null);
+const page = ref(1);
+const fetched = ref(false);
 const fetching = ref(false);
 
-useMeta({ title: `Andi Liang's Posts Page` })
+useMeta({title: `Andi Liang's Posts Page`})
 
-function fetch(page) {
-  if (page > lastPage.value) {
-    return;
-  }
-  if (page !== 1) {
-    fetching.value = true;
-  }
+function fetch() {
 
-  axios
-    .get(`/api/posts?page=${page}`)
-    .then(function (response) {
-      if (response.data.next_page_url === null) {
-        noMorePost.value = true;
-      }
-
-      posts.value.push(...response.data.data);
-      lastPage.value = response.data.last_page;
-    })
-    .catch(function (error) {
-      alert('remote server response with: ' + error.response.status);
-      console.log(error);
-    });
+    fetching.value = true
+    axios
+        .get(`/api/posts?page=${page.value}`)
+        .then(function (response) {
+            fetching.value = false
+            lastPage.value = response.data.last_page
+            posts.value.push(...response.data.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    fetched.value = true
 }
 
-fetch(1);
+
+function loadMore() {
+    // console.log('loading more')
+    if (lastPage.value === null || page.value < lastPage.value) {
+        page.value++;
+    }
+}
+
+watch(page, () => {
+    fetch()
+})
+
+fetch();
+
 </script>
