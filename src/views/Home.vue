e5afb506bb967a88ce60678de104087baab3b204
andiLeong <alwayssmile20150724@gmail.com>
2022-07-06 10:26:11 +0800

    added type text feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,69 +7,77 @@
                         <p class="dark:text-white" v-if="userStore.isLoggedIn">welcome {{ userStore.user.name }}</p>
                         <p class="dark:text-white" v-if="userStore.isAdmin">welcome admin</p>
 
-                        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+                        <span id="me" class="bg-gradient-to-r from-sky-400 to-purple-600 text-transparent bg-clip-text mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
                             Hi, I am andi liang,
-                        </p>
+                        </span>
                     </div>
                 </div>
-                <div class="relative">
-                    <div class="px-4 py-2 relative dark:bg-gray-600 rounded bg-zinc-100 md:p-6">
-                        <div class="lg:grid lg:grid-cols-2 lg:gap-6">
-                            <div class="prose prose-indigo prose-lg text-gray-500 dark:text-white lg:max-w-none">
-                                <p>
-                                    A 2 years full stack php <span class="text-red-500 font-semibold dark:text-red-400">laravel</span> +
-                                    <span class="text-green-500 font-semibold">vue</span> web developer.
-                                    Actually this blog was built on top of laravel + vue. Make use of laravel act as
-                                    backend,
-                                    utilities vue router to make SPA to improve user experience.
-                                    For the style I love using utility css framework tailwind.
-                                </p>
 
-                                <h3 class="dark:text-white">Skills</h3>
-                                <div>
-                                    <div v-for="skill in skills" :key="skill" class="flex items-center">
-                                        <div class="mr-2">
-                                            <svg class="h-6 w-6 text-sky-400" xmlns="http://www.w3.org/2000/svg"
-                                                 fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                 aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                                            </svg>
+                <transition enter-active-class="ease-out duration-300"
+                            enter-from-class="opacity-0"
+                            enter-to-class="opacity-100"
+                            leave-active-class="ease-in duration-200"
+                            leave-from-class="opacity-100"
+                            leave-to-class="opacity-0">
+                    <div v-show="show" class="relative">
+                        <div class="px-4 py-2 relative dark:bg-gray-600 rounded bg-zinc-100 md:p-6">
+                            <div class="lg:grid lg:grid-cols-2 lg:gap-6">
+                                <div class="prose prose-indigo prose-lg text-gray-500 dark:text-white lg:max-w-none">
+                                    <p>
+                                        A 2 years full stack php <span class="text-red-500 font-semibold dark:text-red-400">laravel</span> +
+                                        <span class="text-green-500 font-semibold">vue</span> web developer.
+                                        Actually this blog was built on top of laravel + vue. Make use of laravel act as
+                                        backend,
+                                        utilities vue router to make SPA to improve user experience.
+                                        For the style I love using utility css framework tailwind.
+                                    </p>
+
+                                    <h3 class="dark:text-white">Skills</h3>
+                                    <div>
+                                        <div v-for="skill in skills" :key="skill" class="flex items-center">
+                                            <div class="mr-2">
+                                                <svg class="h-6 w-6 text-sky-400" xmlns="http://www.w3.org/2000/svg"
+                                                     fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                     aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                                </svg>
+                                            </div>
+                                            <div>{{ skill }}</div>
                                         </div>
-                                        <div>{{ skill }}</div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="mt-6 prose prose-indigo prose-lg text-gray-500 dark:text-white lg:mt-0">
-                                <h3 class="dark:text-white">Advantage</h3>
+                                <div class="mt-6 prose prose-indigo prose-lg text-gray-500 dark:text-white lg:mt-0">
+                                    <h3 class="dark:text-white">Advantage</h3>
 
-                                <div class="space-y-2">
-                                    <div v-for="advantage in advantages" :key="advantage"> {{advantage}}</div>
-                                </div>
+                                    <div class="space-y-2">
+                                        <div v-for="advantage in advantages" :key="advantage"> {{advantage}}</div>
+                                    </div>
 
-                                <h3  class="dark:text-white">Experience</h3>
-                                <p>
-                                    Maintain and develop several projects from previous company, that includes a small
-                                    ecommerce site,
-                                    a job posting site, A social insurance buying platform, a construction monitor
-                                    platform
-                                </p>
+                                    <h3  class="dark:text-white">Experience</h3>
+                                    <p>
+                                        Maintain and develop several projects from previous company, that includes a small
+                                        ecommerce site,
+                                        a job posting site, A social insurance buying platform, a construction monitor
+                                        platform
+                                    </p>
 
-                                <h3  class="dark:text-white">Code Review</h3>
-                                <p class="sticky-nav">
-                                    had done some side code project where you can check and view my code style
-                                    including:
-                                </p>
-                                <div ref="stickyNav"></div>
-                                <div class="space-y-0">
-                                    <div v-for="review in codeReviews" :key="review.name">
-                                        <a class="text-sky-400 dark:text-blue-400" :href="review.url">{{review.name}}</a>
+                                    <h3  class="dark:text-white">Code Review</h3>
+                                    <p class="sticky-nav">
+                                        had done some side code project where you can check and view my code style
+                                        including:
+                                    </p>
+                                    <div ref="stickyNav"></div>
+                                    <div class="space-y-0">
+                                        <div v-for="review in codeReviews" :key="review.name">
+                                            <a class="text-sky-400 dark:text-blue-400" :href="review.url">{{review.name}}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </transition>
             </div>
         </div>
     </div>
@@ -80,10 +88,12 @@ import {useUserStore} from '@/store/user'
 import {onMounted, ref} from "vue";
 import {useStickyNavStore} from "@/store/stickyNav";
 import {useMeta} from "vue-meta";
+import TypeIt from "typeit";
 
 const userStore = useUserStore();
 const useStickNavStore = useStickyNavStore();
 const stickyNav = ref()
+const show = ref(false)
 
 const skills = ref([
     'native php',
@@ -128,6 +138,14 @@ onMounted( () => {
         }
     },{threshold:.25})
     observer.observe(stickyNav.value)
+
+
+    new TypeIt("#me", {
+        afterComplete: async () => {
+            show.value = true;
+        },
+    }).go();
+
 })
 
 
